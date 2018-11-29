6082425cecd71aa65a722a7a1e21f281c4e93881
Timofey Remeslennikov <superxxxboom@gmail.com>
2018-11-29 19:18:44 +0300

    Add prefix and z-functions

---
+++ source/strings/prefix_function.cpp
@@ -0,0 +1,12 @@
+vi prefix_function(string s) {
+	int n = sz(s);
+	vi pi (n);
+	for (int i = 1; i < n; ++i) {
+		int j = pi[i - 1];
+		while (j > 0 && s[i] != s[j])
+			j = pi[j - 1];
+		if (s[i] == s[j]) ++j;
+		pi[i] = j;
+	}
+	return pi;
+}
